ca1c25a32033fa6df81ff619b0c876023125f98e
Diego Lapiduz <dlapiduz@users.noreply.github.com>
2017-05-18 17:24:09 -0500

    Extract from the right paths

--- ci/tasks/create-release.sh
+++ ci/tasks/create-release.sh
@@ -2,7 +2,14 @@
 
 set -e
 
-./extract.sh
+pushd prometheus-custom-git
+./extract.sh ../prometheus-release-git
+
+git config --global user.email "releasebot@c0.pivotal.io"
+git config --global user.name "Releasebot"
 
 git add . && git commit -m "New release"
 
+popd
+
+cp -r prometheus-custom-git/. pushme
