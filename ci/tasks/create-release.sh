604f1a82636ddd9088feafcd94e9948d952e72d7
Diego Lapiduz <dlapiduz@users.noreply.github.com>
2017-05-22 21:15:33 -0500

    Pipeline and task changes

--- ci/tasks/create-release.sh
+++ ci/tasks/create-release.sh
@@ -8,6 +8,16 @@ pushd prometheus-custom-git
 git config --global user.email "releasebot@c0.pivotal.io"
 git config --global user.name "Releasebot"
 
+cat > config/private.yml << EOF
+---
+blobstore:
+  s3:
+    access_key_id: ${AWS_ACCESS_KEY}
+    secret_access_key: ${AWS_SECRET_KEY}
+EOF
+
+bosh -n create release --final --with-tarball --version "${VERSION}"
+
 git add . && git commit -m "New release"
 
 popd
