1187cca24d9580a11eab2666f2236f9bec6b652e
Diego Lapiduz <dlapiduz@users.noreply.github.com>
2017-05-24 14:24:55 -0500

    Dont delete all packages just upstream ones

--- extract.sh
+++ extract.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Clean out old version
-rm -rf packages/* src/* jobs/*
-
 # Get all jobs for alerts and dashboards
 jobs=$(find $1/jobs -name "*_alerts")
 jobs="$jobs
@@ -11,6 +8,8 @@ $(find $1/jobs -name "*_dashboards")"
 # Extract them
 for job in $jobs; do
     echo $job
+    job_name=$(basename $job)
+    rm -rf packages/$job_name jobs/$job_name src/$job_name
     bosh-gen extract-job "$job"
 done
 
